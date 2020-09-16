e9daaf56ad62b330f08d6cb735edfc040df77a1a
salah_besbes <salah_besbes@ymail.com>
2020-09-16 21:43:08 +0100

    Write a C program that prints exactly blablabla

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(void)
 {
-printf "%s with proper grammar, but the outcome is a piece of art,\n";
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
