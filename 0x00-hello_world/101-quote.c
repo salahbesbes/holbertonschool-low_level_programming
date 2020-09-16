c8725337e0495e4e78cfc9c6080119c79a551e20
salah_besbes <salah_besbes@ymail.com>
2020-09-16 22:56:00 +0100

    print some thing without using prinf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 **/
 int main(void)
 {
-char[] message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message));
 return (1);
 }
