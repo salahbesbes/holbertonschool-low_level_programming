377cefb39dcd0e351c11b41b929b8af43fe41ee7
salah_besbes <salah_besbes@ymail.com>
2020-09-16 22:51:02 +0100

    print some thing without using prinf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include <unistd.h>
+/**
+* main - print string
+* Return: 1 (Success)
+**/
 int main(void)
 {
-char[] message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char[] message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message));
 return (1);
 }
