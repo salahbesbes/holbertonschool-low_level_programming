db4b5637883ea27dede170b28ca5f3201b3d000d
salah_besbes <salah_besbes@ymail.com>
2020-09-16 21:05:36 +0100

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
+#include <string.h>
 
-#include<stdio.h> 
-int main() 
-{ 
-    puts("\"Programming is like building a multilingual puzzle"); 
-    
-      
-    getchar(); 
-    return 0; 
-} 
+int main(void)
+{
+char str1[15];
+strcpy(str1, "\"holbertonschool-low_level_programming");
+puts(str1);
+return (0);
+}
