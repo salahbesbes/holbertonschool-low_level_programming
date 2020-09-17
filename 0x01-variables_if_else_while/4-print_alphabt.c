eeb719bb4a88f11c6bef1dffc1fc0c136f20b8b7
salah_besbes <salah_besbes@ymail.com>
2020-09-17 21:30:52 +0100

    print alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - blablabla
+ *
+ * Return: blablabla
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'  ; i++)
+{
+if ( i != 'e' && i != 'q')
+putchar(i);   
+}
+putchar('\n');
+return (0);
+}
