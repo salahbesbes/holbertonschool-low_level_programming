18d62636bdcf2d24ae25e0ebad4f6a3470f9c9a1
salah_besbes <salah_besbes@ymail.com>
2020-09-17 22:15:16 +0100

    print number with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - blablabla
+ *
+ * Return: blablabla
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar((i % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
