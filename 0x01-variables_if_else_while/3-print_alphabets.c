d3067303a8463a7fd4ff99bf06680a9a69d3c232
salah_besbes <salah_besbes@ymail.com>
2020-09-17 21:04:48 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - blablabla
+ *
+ * Return: blablabla
+ */
+int main(void)
+{
+char i;
+for(i='a'; i<='z'; i++)
+{putchar(i);}
+for(i='A'; i<='Z'; i++)
+{putchar(i);}
+putchar('\n');
+return(0);
+}
