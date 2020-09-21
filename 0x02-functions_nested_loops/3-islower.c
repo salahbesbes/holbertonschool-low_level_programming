b4a737f01436d38e63e67d35c40cd5cb71c6bf9c
salah_besbes <salah_besbes@ymail.com>
2020-09-21 14:57:11 +0100

    check for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,8 +8,11 @@
 int _islower(int c)
 {
 	int result;
-	if ( c >= 97  && c <= 122)
-		result = 1;	
-	else result = 0;
+
+	if (c >= 97  && c <= 122)
+		result = 1;
+	else
+		result = 0;
 	return (result);
 }
+
