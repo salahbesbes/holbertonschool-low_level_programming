436e46f8383aa12937c689d330f676c021aaf010
salah_besbes <salah_besbes@ymail.com>
2020-09-22 13:45:36 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,102 +1,28 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - check the code for Holberton School students.
- * @n: blablabla
- *
- * Return: nb to 98
- * caseLessAndPositive - blablabla
- * description: blablbalba
- * @n: blablabal
- *
+ * largest_number - returns the largest of 3 numbers
+ * @n: first integer
+ * Return: largest number
  */
 
-void caseLessAndPositive(int n)
-{
-	int i, x, y, z;
-
-	for (i = n; i <= 98; i++)
-	{
-		z = i / 100;
-		x = i / 10;
-		y = i % 10;
-		if (i >= 100)
-			_putchar(z + '0');
-		if (i >= 10)
-			_putchar(x + '0');
-		_putchar(y + '0');
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-}
-
-void caseGreater(int n)
-{
-	int i, x, y, z;
-
-	for (i = n; i >= 98; i--)
-	{
-		z = i / 100;
-		x = i / 10 % 10;
-		y = i % 10;
-		if (i >= 100)
-			_putchar(z + '0');
-		if (i >= 10)
-			_putchar(x + '0');
-		_putchar(y + '0');
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-
-}
-
-void caseNegative(int n)
-{
-	int i, x, y, z;
-
-	for (i = n; i <= 98; i++)
-	{
-		z = i / 100;
-		x = i / 10 % 10;
-		y = i % 10;
-		if (i < 0)
-		{
-			_putchar('-');
-			if (z < 0)
-				_putchar(z * -1 + '0');
-			if (x < 0)
-				_putchar(x * -1 + '0');
-			_putchar(y * -1 + '0');
-		}
-		else
-		{
-			if (i >= 100)
-				_putchar(z + '0');
-			if (i >= 10)
-				_putchar(x + '0');
-			_putchar(y + '0');
-		}
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-}
 void print_to_98(int n)
 {
-	if (n <= 98 && n >= 0)
-		caseLessAndPositive(n);
-	if (n > 98)
-		caseGreater(n);
-	if (n < 0)
-		caseNegative(n);
-	_putchar('\n');
+	int i;
+	if (n >= 98)
+	{
+		for(i = n; i >= 98; i--)
+		{
+			printf("%d, ",i);
+		}
+	}
+	else 
+	{
+		for(i = n; i <= 98; i++)
+		{
+			printf("%d, ",i);
+		}
+	}
+	printf("\n");
 }
 
