efcedc2b8e9686ea5bb71872e5cadaf9caca90ad
salah_besbes <salah_besbes@ymail.com>
2020-09-22 22:59:16 +0100

    hh

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,7 +5,7 @@
  * Return: Always 0.
  */
 
-void main(void)
+int main(void)
 {
 	char str[] = "Holberton";
 	int i;
@@ -15,5 +15,6 @@ void main(void)
 		_putchar(str[i]);
 	}
 	_putchar('\n');
+	return (0);
 }
 
