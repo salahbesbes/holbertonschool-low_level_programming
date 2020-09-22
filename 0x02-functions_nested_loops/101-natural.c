3f5ef5e533acb6547c8090e93149b113a2fa9212
salah_besbes <salah_besbes@ymail.com>
2020-09-22 16:45:06 +0100

    gg

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,14 @@
  */
 void main(void)
 {
-	int i, sum1 = 0, sum2 = 0, resultat;
+	int i, sum1 = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0 || i % 5 == 0) && i % 15 != 0)
 			sum1 += i;
-		if (i % 5 == 0 && i % 3 != 0)
-			sum2 += i;
 	}
-	resultat = sum1 + sum2;
-	printf("%d\n",resultat);
+	
+	printf("%d\n",sum1);
 }
 
