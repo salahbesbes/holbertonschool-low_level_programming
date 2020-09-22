7234a8c6a247d3ad02abb028438e7d672179910f
salah_besbes <salah_besbes@ymail.com>
2020-09-22 10:52:13 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,20 +1,12 @@
 #include "holberton.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char str[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
-
