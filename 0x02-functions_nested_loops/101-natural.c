fadd7c0b23d124a63142f87344d6c3ea83da838f
salah_besbes <salah_besbes@ymail.com>
2020-09-22 16:15:14 +0100

    natural numbers < 1024 pultiple of 5 and 3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,13 @@ void main(void)
 {
 	int i, res = 0;
 
-	for(i = 0; i < 30; i++)
+	for (i = 0; i < 30; i++)
 	{
-		if (i % 5 == 0 && i % 3 != 0)
-			res += i;
-		if (i % 3 == 0 && i % 5 != 0)
-			res += i;
+	if (i % 5 == 0 && i % 3 != 0)
+		res += i;
+	if (i % 3 == 0 && i % 5 != 0)
+		res += i;
 	}
-	printf("%d \n",res);
-	
+	printf("%d \n", res);
 }
+
