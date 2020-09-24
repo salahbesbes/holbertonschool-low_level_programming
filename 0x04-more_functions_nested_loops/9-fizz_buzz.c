1c402213fa3979957ca3eb425ee810b9ed753cb2
salah_besbes <salah_besbes@ymail.com>
2020-09-24 18:26:53 +0100

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,21 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%d ", i);
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
 	}
 	printf("\n");
 
