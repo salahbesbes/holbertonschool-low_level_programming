6b3b2f534b072e13610e28e04882ba383ec8436d
salah_besbes <salah_besbes@ymail.com>
2020-09-24 13:29:11 +0100

    digit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i > 10)
+				_putchar( i / 10 + '0');
+			_putchar( i % 10 + '0');
 		}
 		_putchar('\n');
 	}
