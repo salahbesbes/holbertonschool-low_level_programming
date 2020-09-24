2b99b8add5f14fc1b45518cf4d7ccbf36d98690c
salah_besbes <salah_besbes@ymail.com>
2020-09-24 09:10:18 +0100

    check for Upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int i, result;
+	if ('c' >= 'A' && 'c'<= 'Z')
+		result = 1;
+	result = 0;
+	return result;
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+
+#More functions, more nested loops
