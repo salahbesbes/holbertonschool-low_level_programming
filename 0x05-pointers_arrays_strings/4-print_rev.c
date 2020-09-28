7fceadbc26ce3ed44134bf339c8931d13562aaa0
salah_besbes <salah_besbes@ymail.com>
2020-09-28 14:05:47 +0100

    print string int reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+# include "holberton.h"
+
+
+/**
+ * print_rev - print str in reverse
+ * @s: string
+ * Return: string
+ */
+
+
+void print_rev(char *s)
+{
+	int i, length;
+
+	for (i = 0; s[i]; i++)
+	{
+		length++;
+	}
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+
+}
