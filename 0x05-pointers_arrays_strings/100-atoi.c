de76f78a84a1e614bb69957a1bd7cc4b2e3f6774
salah_besbes <salah_besbes@ymail.com>
2020-09-29 22:39:58 +0100

    convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 
 /**
@@ -11,6 +12,29 @@
 
 int _atoi(char *s)
 {
-	_putchar(*s);
-	return (0);
+
+	int i ,j, k, num = 0, firstNumber = 1, negatif = 1;
+
+	i = j = k = 0;
+	while(s[j])
+	{
+
+		if (s[j] == '-' && !num)
+			negatif *= -1;
+		if (s[j] >= '0' && s[j] <= '9' && firstNumber)
+		{
+			num *= 10;
+			num += s[j] - '0';
+		}
+		else if (num > 0)
+			firstNumber = 0;
+
+		if (s[j] == '-' && !num)
+			num *= -1;
+
+		j++;
+	};
+
+	num *= negatif;
+	return (num);
 }
