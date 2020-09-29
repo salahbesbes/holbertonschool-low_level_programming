04b3f13d9da0d9b4249943ce93508ff358320ff5
salah_besbes <salah_besbes@ymail.com>
2020-09-29 11:48:47 +0100

    print str

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,20 +7,17 @@
  * Return: void
  */
 
-
 void puts2(char *str)
 {
 	int i, length;
-	char *p = str;
 
 	for (i = 0; str[i]; i++)
 		length++;
 
 	for (i = 0; i < length; i++)
 	{
-		if (i  % 2 == 0)
-			_putchar(*p);
-		p++;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
