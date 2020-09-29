1ce822c573b78f7468e5326f68952e6debefe33d
salah_besbes <salah_besbes@ymail.com>
2020-09-29 12:11:22 +0100

    gg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 
 	if (length % 2 == 1)
 	{
-		for (i = ((length - 1) / 2); i < length; i++)
+		for (i = ((length - 1) / 2) - 1; i < length; i++)
 		{
 			_putchar(str[i]);
 
