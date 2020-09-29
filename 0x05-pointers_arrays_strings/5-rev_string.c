d90e9bb48ed55be67d07266ce23ece747e28087e
salah_besbes <salah_besbes@ymail.com>
2020-09-29 09:52:07 +0100

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,16 +12,29 @@ void rev_string(char *s)
 
 	for (i = 0; s[i]; i++)
 		length++;
-	char *start = s, *end = s + length - 1, tmp;
+	/*
+	 * char *start = s, *end = s + length - 1, tmp;
+	 */
+	char tmp;
 
 	for (i = 0; i < length / 2; i++)
 	{
-		tmp = *end;
-		*end = *start;
-		*start = tmp;
-		start++;
-		end--;
+		/*
+		 *tmp = *end;
+		 *end = *start;
+		 *start = tmp;
+		 *start++;
+		 *end--;
+		 */
+
+		tmp = *(s + length - 1 - i);
+		*(s + length - 1 - i) = *(s + i);
+		*(s + i) = tmp;
+
 	}
+
+
+
 }
 
 
