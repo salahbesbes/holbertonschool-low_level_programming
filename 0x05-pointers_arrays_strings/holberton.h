003540adc198099167f5e46fcf8056b108abbac1
salah_besbes <salah_besbes@ymail.com>
2020-09-29 10:04:11 +0100

    holberton

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -8,4 +9,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char * _strcpy(char *dest,const char *src);
+
 int _putchar(char c);
