d4f8310d2bc2165e44ad923e1e14da5bff9383d3
salah_besbes <salah_besbes@ymail.com>
2020-09-29 12:58:54 +0100

    gg

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+/**
+ * _atoi - transform string to entiger
+ * @s: string
+ *
+ * Return: integer
+ */
+
+
+int _atoi(char *s)
+{
+	return (0);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -9,5 +9,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _atoi(char *s);
 int _putchar(char c);
