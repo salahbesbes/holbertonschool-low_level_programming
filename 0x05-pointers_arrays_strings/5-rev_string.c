5ff330e54e559245fe714bc0124213611e09a50f
salah_besbes <salah_besbes@ymail.com>
2020-09-29 11:37:51 +0100

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,15 @@
 
 #include "holberton.h"
-
+/**
+ * rev_string - everses a string.
+ * @s: string
+ * return: string
+ */
 void rev_string(char *s)
 {
 	int length = 0, i;
 	char tmp;
+
 	for (i = 0; s[i]; i++)
 	{
 		length++;
