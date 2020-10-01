176c570da0b01425f7a54b8c6ebb6d82f051900e
salah_besbes <salah_besbes@ymail.com>
2020-10-01 14:13:19 +0100

    capitalise synthese

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize every string after some special carachters
+ * @str: string ( senthense )
+ *
+ * Return: new Synthese with capitalize words
+ */
+
+
+char *cap_string(char *str)
+{
+
+	int specialCaracter[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '\"', '(', ')', '{', '}'};
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == specialCaracter[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+
+		}
+	}
+
+
+	return (str);
+}
