9240a3ed063ab4c6bffb2c681ba588d535e886b4
salah_besbes <salah_besbes@ymail.com>
2020-10-01 09:38:07 +0100

    Cocat 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: str
+ * @src: str
+ *
+ * Return: String
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	for (j = 0; dest[j]; j++)
+		;
+	for (i = 0; src[i]; i++, j++)
+	{
+		dest[j] = src[i];
+	}
+	return (dest);
+}
