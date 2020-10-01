c5777b596bd203e5fbe231d3d5ef46c1cf8e8731
salah_besbes <salah_besbes@ymail.com>
2020-10-01 13:30:18 +0100

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse and array
+ * @a: array
+ * @n: integer
+ *
+ * Return: reversed array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	for (i = 0; i < n / 2; i ++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 -i] = tmp;
+	}
+}
