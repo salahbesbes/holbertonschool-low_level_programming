bdc7c7181c92c1465daa366cec000549d24cbe4c
salah_besbes <salah_besbes@ymail.com>
2020-10-01 12:56:37 +0100

    compare 2 string by their code escii

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, diff, foundDiff = 0;
+	int i, diff = 0, foundDiff = 0;
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; (s1[i] || s2[i]) && !foundDiff ;  i++)
 	{
-		if (s1[i] != s2[i] && !foundDiff)
-		{
-			diff = s1[i] - s2[i];
+		diff = s1[i] - s2[i];
+		if (diff != 0)
 			foundDiff = 1;
-		}
-		else if (s1[i] == s2[i] && !foundDiff)
-		{
-			diff = 0;
-			foundDiff = 1;
-		}
+
 	}
 	return (diff);
 
