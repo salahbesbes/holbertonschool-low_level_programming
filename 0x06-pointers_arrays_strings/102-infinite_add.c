5357f4fe9276a87fb1ef20d20054bd60e61319e5
salah_besbes <salah_besbes@ymail.com>
2020-10-01 21:50:52 +0100

    infinit add

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - description
+ * @n1: str
+ * @n2: str
+ * @r: str
+ * @size_r: int
+ *
+ * Return: string
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
