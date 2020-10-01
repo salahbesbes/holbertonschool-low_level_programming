20cb19be984ea9e0b5be0d0c1cf7afb1c541f5ee
salah_besbes <salah_besbes@ymail.com>
2020-10-01 21:30:49 +0100

    print number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_number - check the code for Holberton School students.
+ * @n: integer
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+
+
+	int i = 1;
+	unsigned int num;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	num = n;
+	do {
+		i *= 10;
+		n /= 10;
+	} while (n);
+	i /= 10;
+	do {
+		_putchar(num / i + '0');
+		num %= i;
+		i /= 10;
+	} while (num);
+}
