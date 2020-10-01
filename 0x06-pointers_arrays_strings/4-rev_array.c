603565ccaefc464c25139973aa6f49a704965ef5
salah_besbes <salah_besbes@ymail.com>
2020-10-01 13:31:59 +0100

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,11 @@
 void reverse_array(int *a, int n)
 {
 	int i, tmp;
-	for (i = 0; i < n / 2; i ++)
+
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 -i] = tmp;
+		a[n - 1 - i] = tmp;
 	}
 }
