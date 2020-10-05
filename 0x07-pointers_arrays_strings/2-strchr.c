2d1da90f5e11989ea6a394e684f6b3d2e5d0bc12
salah_besbes <salah_besbes@ymail.com>
2020-10-05 23:01:39 +0100

    locate str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,14 +13,15 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	if (c == '\0')
-		return (s);
 
 	for (i = 0; s[i]; ++i)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
+
+	if (c == '\0')
+		return (s + i);
 	return (0);
 }
 
