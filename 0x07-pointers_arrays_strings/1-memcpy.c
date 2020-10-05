4bfa74c4095ecf0b555750683666851b94adab40
salah_besbes <salah_besbes@ymail.com>
2020-10-05 13:59:20 +0100

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n && src[i]; ++i)
+	for (i = 0; i < n ; ++i)
 	{
-
 		dest[i] = src[i];
 	}
 	return (dest);
