e932467956fd6f4a674e1d9d7e80e7bcf2235049
salah_besbes <salah_besbes@ymail.com>
2020-10-07 22:52:10 +0100

    primer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,39 @@
 #include "holberton.h"
 #include <stdio.h>
 
+
+
+
+/**
+ * squereRoot - SQRT of int
+ * @i: int
+ *
+ * Return: int
+ */
+
+int squereRoot(int i)
+{
+	int tmp = 0, res;
+
+	tmp = i;
+	if (i < 0)
+		return (-1);
+
+	if (i == 1)
+		return (1);
+
+
+	if (i % 2 == 0)
+		res = i / squereRoot((i / tmp + tmp) / 2);
+	else
+		res = i / (squereRoot((i / tmp + tmp) / 2) + 1);
+/*	printf("|res %d, n = %d, tmp = %d,\n",res ,n ,tmp);*/
+	return (res);
+
+}
+
+
+
 /**
  * is_prime_number - prime number
  * @n: number
@@ -8,12 +41,24 @@
  * Return: 0 or 1
  */
 
-
-
-
-
 int is_prime_number(int n)
 {
+	int composit = 2, qt, res;
 
-	
+	qt = squereRoot(n);
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else if (n % 2 == 0)
+		return (0);
+	else if (n % 5 == 0)
+		return (0);
+	else
+	{
+		n--;
+		res = (1 +  is_prime_number(n));
+
+	}
+	return (res);
 }
