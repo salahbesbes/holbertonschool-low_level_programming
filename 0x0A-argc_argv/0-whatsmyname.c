6854e241131dca0f18306fa4fc90f4b7029cef5c
salah_besbes <salah_besbes@ymail.com>
2020-10-10 13:46:17 +0100

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#ifdef __GNUC__
+#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#else
+#  define UNUSED(x) UNUSED_ ## x
+#endif
+
+#ifdef __GNUC__
+#  define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_ ## x
+#else
+#  define UNUSED_FUNCTION(x) UNUSED_ ## x
+#endif
 
 /**
 * main - print the name of the execution file
@@ -11,7 +22,7 @@
 * Error: 1
 */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int UNUSED(argc), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
