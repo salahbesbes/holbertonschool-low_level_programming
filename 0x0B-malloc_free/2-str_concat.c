c85653f66e4ff465adb676c26b2cc6fc81416ee1
salah_besbes <salah_besbes@ymail.com>
2020-10-13 15:36:20 +0100

    concat 2 str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,12 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, lenS1, lenS2;
 	char *p;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	for (lenS1 = 0; s1[lenS1]; lenS1++)
 	;
 	for (lenS2 = 0; s2[lenS2]; lenS2++)
@@ -35,5 +36,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s2[i]; i++, j++)
 		p[j] = s2[i];
 	p[j] = '\0';
+
+
 	return (p);
 }
