c4900fcf59b0151fedb787a95287d1c12c70b755
salah_besbes <salah_besbes@ymail.com>
2020-10-14 15:38:11 +0100

    concat 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lenS1 = 0, lenS2 = 0, totalLength = 0;
+	unsigned int lenS1 = 0, totalLength = 0;
 	unsigned int i, j;
 	char *p;
 
