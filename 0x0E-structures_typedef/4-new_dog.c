dbb6d66a77eae6da7c4e65a1609584bcc5f7508b
salah_besbes <salah_besbes@ymail.com>
2020-10-19 15:40:55 +0100

    create new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
+	char *s_name, *s_owner;
+	int i, lenName, lenOwner;
 
 	newDog = malloc(sizeof(dog_t));
-	newDog->name = name;
+	if (!newDog)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	for (lenName = 0; name[lenName]; lenName++)
+	;
+	s_name = malloc(sizeof(*s_name) * (lenName + 1));
+	if (!s_name)
+	{
+		free(s_name);
+		free(newDog);
+		return (NULL);
+	}
+	for (lenOwner = 0; owner[lenOwner]; lenOwner++)
+	;
+	s_owner = malloc(sizeof(char) * (lenOwner + 1));
+	if (!s_owner)
+	{
+		free(s_owner);
+		free(s_name);
+		free(newDog);
+		return (NULL);
+	}
+
+	for (i = 0; name[i]; i++)
+		s_name[i] = name[i];
+	s_name[i] = '\0';
+
+	newDog->name = s_name;
 	newDog->age = age;
-	newDog->owner = owner;
+
+	for (i = 0; owner[i]; i++)
+		s_owner[i] = owner[i];
+	s_owner[i] = '\0';
+
+	newDog->owner = s_owner;
 	return (newDog);
 }
