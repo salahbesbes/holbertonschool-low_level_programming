9be10cb75bef05323880ad70b0b2b14fcf53f3ba
salah_besbes <salah_besbes@ymail.com>
2020-10-19 14:02:05 +0100

    new type def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: float -> his age
  * @owner: str -> his owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
