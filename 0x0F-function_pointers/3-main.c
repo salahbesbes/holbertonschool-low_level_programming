a37e4ab74dd607b139b1d9529bf47f9efadc2418
salah_besbes <salah_besbes@ymail.com>
2020-10-21 20:00:03 +0100

    calulator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
 
 	int (*result)(int, int);
 
