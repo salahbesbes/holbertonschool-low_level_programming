781a4d12dd798ab54c8b0f97026096c3194a3e76
salah_besbes <salah_besbes@ymail.com>
2020-10-24 15:45:48 +0100

    header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef GARD
+#define GARD
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void _putchar(char ch);
+
+#endif
