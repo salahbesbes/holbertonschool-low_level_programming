fb422742ca92219bb0426a1e5c8d3c4267c3f1d4
salah_besbes <salah_besbes@ymail.com>
2020-10-25 12:37:51 +0100

    print all args

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,54 @@
 #include <stdarg.h>
 
 /**
+* switchCASE -  helping the mainfunction to switch case
+* @vaList: va_list
+* @j: int index
+* @format: const char
+*
+* Return: void
+*/
+
+void switchCASE(va_list vaList, int j, const char *format)
+{
+	char *separator = ", ", *str;
+
+	if (j == 0)
+		separator = "";
+	switch (format[j])
+		{
+			case 'c':
+				{
+					printf("%s%c", separator, va_arg(vaList, int));
+					break;
+				}
+			case 'i':
+				{
+					printf("%s%d", separator, va_arg(vaList, int));
+					break;
+				}
+			case 'f':
+				{
+					printf("%s%.2f ", separator, va_arg(vaList, double));
+					break;
+				}
+			case 's':
+				{
+					str = va_arg(vaList, char*);
+					if (str)
+					{
+						printf("%s%s", separator, str);
+						break;
+					}
+					printf("%s(nil)", separator);
+					break;
+				}
+			default:
+				break;
+		}
+}
+
+/**
 * print_all - print different type of variable
 * @format: is a string that contain the first letter
 * representing a type to be printed
@@ -13,46 +61,13 @@
 void print_all(const char *const format, ...)
 {
 	va_list vaList;
-	char *str;
-	int nb, j, i;
-	double fl;
+	int j;
 
 	va_start(vaList, format);
 	j = 0;
 	while (format[j])
 	{
-		switch (format[j])
-		{
-			case 'c':
-				{
-					i = va_arg(vaList, int);
-					printf("%c ", i);
-					break;
-				}
-			case 'i':
-				{
-					nb = va_arg(vaList, int);
-					printf("%d ", nb);
-					break;
-				}
-			case 'f':
-				{
-					fl = va_arg(vaList, double);
-					printf("%.2f ", fl);
-					break;
-				}
-			case 's':
-				{
-					str = va_arg(vaList, char*);
-					if (str)
-						printf("%s ", str);
-					if (!str)
-						printf("(nil)");
-					break;
-				}
-			default:
-				break;
-		}
+		switchCASE(vaList, j, format);
 	j++;
 	}
 	printf("\n");
