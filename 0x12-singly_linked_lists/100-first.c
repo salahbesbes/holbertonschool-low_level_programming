26eb58f50ff01b89669113b6dceeedc69f94f280
salah_besbes <salah_besbes@ymail.com>
2020-10-29 00:52:53 +0100

    execute before main fuc

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - stats before main func
+ *
+ * Return: void
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
+
