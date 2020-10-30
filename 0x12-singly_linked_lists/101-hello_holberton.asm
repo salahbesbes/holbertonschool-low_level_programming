19452bd4f9d9a625eb8b0baf2514dc780e0d3738
salah_besbes <salah_besbes@ymail.com>
2020-10-30 19:41:41 +0100

    print hello woth assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,14 @@
-
 SECTION .data
-	arg1:	db "Hello, Holberton", 0
-	fmt:     db "%s", 0xA, 0
+	message: db "Hello, Holberton", 0
+	format:  db "%s", 0xA, 0
 
 SECTION .text
 extern printf
 global main
 
 main:
-	mov esi, arg1
-	mov rdi, fmt
+	mov esi, message
+	mov rdi, format
 	mov eax, 0
 	call printf
 
@@ -17,3 +16,4 @@ main:
 	mov ebx, 0
 	mov eax, 1
 	int 0x80
+
