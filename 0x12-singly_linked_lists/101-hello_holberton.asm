15a961643eebfade6ffbe3e7738b497c02cebedc
salah_besbes <salah_besbes@ymail.com>
2020-10-30 19:34:50 +0100

    print hello woth assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,14 +7,13 @@ SECTION .text
 extern printf
 global main
 
-	main:
-		mov esi, arg1
-		mov rdi, fmt
-		mov eax, 0
-		call printf
+main:
+	mov esi, arg1
+	mov rdi, fmt
+	mov eax, 0
+	call printf
 
 
-		mov ebx, 0
-		mov eax, 1
-
-		int 0x80
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
