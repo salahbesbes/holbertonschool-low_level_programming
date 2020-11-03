712ae4466f96f246b9a7b8499b2c4284fb07289d
salah_besbes <salah_besbes@ymail.com>
2020-11-03 01:13:52 +0100

    print all linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+* print_listint - print all linked list and return nb of nodes
+* @h: head pointer to the first node
+*
+* Return: nb of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
