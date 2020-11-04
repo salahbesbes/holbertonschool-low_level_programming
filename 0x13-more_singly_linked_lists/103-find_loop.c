252c5eedb42bf70e1585ebcdb144613216c721df
salah_besbes <salah_besbes@ymail.com>
2020-11-04 23:46:23 +0100

    find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+
+
+/**
+* find_listint_loop - check if ther is a loop in a list 
+* @head: header pointer
+*
+* Return: pointer to the header
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+	listint_t *slower, * faster;
+	slower = head;
+	faster = head->next;
+	while(1)
+	{
+		if( !faster || !faster->next)
+			return (NULL);
+		else if (faster == slower || faster->next == slower)
+			return (slower);
+		else
+		{
+			slower = slower->next;
+			faster = faster->next->next;
+		}
+	}
+}
