c0c19dcc21b0448ef8e6c34cb8f31897e2c681ca
salah_besbes <salah_besbes@ymail.com>
2020-11-04 12:58:54 +0100

    returns the nth node of a list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,16 +13,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	unsigned int i = 0;
 	listint_t *current = NULL;
-	
+
 	if (!head)
 		return (NULL);
 
 	current = head;
 	for (i = 0; current && i < index; i++)
-	{
-		
 		current = current->next;
-	}
+
 	if (!current)
 		return (NULL);
 	return (current);
