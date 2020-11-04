3cccc47d3311c2b55167c6f22504b593a0a78a36
salah_besbes <salah_besbes@ymail.com>
2020-11-04 13:28:04 +0100

    insert at position x

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,34 +6,39 @@
 * insert_nodeint_at_index - insert node at a given index
 * @head: pointerr to the header
 * @idx: index where to insert the node
-* @n: data 
+* @n: data
 * Return: newNode added
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
-	listint_t *newNode, *current;
+	listint_t *newNode = NULL, *current = NULL;
 	unsigned int i;
 
 
-	if (!*head)
+	if (!head)
 		return (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
 		return (NULL);
 
 	newNode->n = n;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
 	current = *head;
 	for (i = 0; current && i < idx - 1; i++)
 		current = current->next;
 
 	if (!current)
 		return (NULL);
-
 	newNode->next = current->next;
 	current->next = newNode;
-	
+
 
 	return (newNode);
 }
