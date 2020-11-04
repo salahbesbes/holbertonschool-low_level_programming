1f4eaff276e9f4e9675847b422c299e789bd003e
salah_besbes <salah_besbes@ymail.com>
2020-11-04 21:23:14 +0100

    print safe list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+* print_listint_safe - print linked list even if its circular
+* @head: pouinter to the heder
+*
+* Return: nb of nodes
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *current, *lastNode, *nextNode;
+	int i, n, found = 0;
+	listint_t *array[50];
+
+	if (!head)
+		return (0);
+	current = (listint_t *)head;
+	n = 0;
+	while (current)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (array[i] == current)
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found)
+		{
+			printf("-> [%p] %d\n", current, current->n);
+			exit(98);
+		}
+		printf("[%p] %d\n", current, current->n);
+		array[n] = current;
+		n++;
+		current = current->next;
+	}
+
+	return (0);
+}
