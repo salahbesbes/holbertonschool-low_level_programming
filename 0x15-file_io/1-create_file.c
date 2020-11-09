b7ba762542157a7e39c33b19323fc0b54249d813
salah_besbes <salah_besbes@ymail.com>
2020-11-09 18:35:54 +0100

    create a file and trancate if exist

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+/**
+* create_file - Create a function that creates a file.
+* @filename: filename
+* @text_content: text_content to appand
+* Return: -1
+* Error: 1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int len = 0;
+	int fileNumber = 0;
+	int charWriten = 0;
+
+	for (len = 0; text_content[len]; len++)
+	;
+
+	if (!filename)
+		return (-1);
+
+	fileNumber = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR + S_IWUSR);
+	if (fileNumber < 0)
+		return (-1);
+	if (!text_content)
+		return (1);
+	charWriten = write(fileNumber, text_content, len);
+	if (charWriten != len)
+		return (-1);
+	return (1);
+}
