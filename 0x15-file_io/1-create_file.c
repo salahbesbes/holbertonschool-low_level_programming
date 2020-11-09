3b452b3c5e2657a5dad792462e2d008164c323d5
salah_besbes <salah_besbes@ymail.com>
2020-11-09 18:48:00 +0100

    create a file and trancate if exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fileNumber = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR + S_IWUSR);
+	fileNumber = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fileNumber < 0)
 		return (-1);
 	if (!text_content)
