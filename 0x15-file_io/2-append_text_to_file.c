be7438f3d7e54e942ee3213b0c7c605152274fff
salah_besbes <salah_besbes@ymail.com>
2020-11-09 19:03:50 +0100

    appand a string into a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+/**
+* append_text_to_file - appand a senthense into a file 
+* @filename: str
+* @text_content: string to be appanded
+* Return: 1
+* Error: -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len = 0;
+	int fileNumber = 0;
+	int charWriten = 0;
+
+	for (len = 0; text_content[len]; len++)
+	;
+
+	if (!filename)
+		return (-1);
+
+	fileNumber = open(filename, O_WRONLY | O_APPEND);
+	if (fileNumber < 0)
+		return (-1);
+	if (!text_content)
+		close(fileNumber);
+	charWriten = write(fileNumber, text_content, len);
+	close(fileNumber);
+	if (charWriten != len)
+		return (-1);
+
+
+
+	return (1);
+}
