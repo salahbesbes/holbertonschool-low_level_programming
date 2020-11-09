09cf32e4ae553c58c7da18de3a36a7f37cf4f9f6
salah_besbes <salah_besbes@ymail.com>
2020-11-09 18:46:44 +0100

    create a file and trancate if exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,9 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (1);
 	charWriten = write(fileNumber, text_content, len);
+	close(fileNumber);
 	if (charWriten != len)
 		return (-1);
+
 	return (1);
 }
