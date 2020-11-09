a9c762ebc98a35e1e7304cf4a036c4c928ca0dc8
salah_besbes <salah_besbes@ymail.com>
2020-11-09 19:28:03 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <asm-generic/errno-base.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -26,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fileNumber = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fileNumber < 0)
+	if (fileNumber < 0 || fileNumber == EACCES)
 		return (-1);
 	if (!text_content)
 		return (1);
