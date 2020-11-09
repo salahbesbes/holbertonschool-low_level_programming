e3d13c79aebbb17973105d09088716a00a5d817d
salah_besbes <salah_besbes@ymail.com>
2020-11-09 22:09:40 +0100

    copy file to an other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,13 +21,13 @@ int main(int argc, char *argv[])
 	int checkExit = 0;
 	char buf[1024];
 
-	if (argc != 4)
+	if (argc != 3)
 		exit(97);
 
-	cpFrom = open(argv[2], O_RDONLY);
+	cpFrom = open(argv[1], O_RDONLY);
 	if (cpFrom < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	while ((readChar = read(cpFrom, buf, 1024)) > 0)
@@ -36,21 +36,21 @@ int main(int argc, char *argv[])
 
 	if (readChar < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	cpTo = open(argv[3], O_WRONLY | O_CREAT | O_APPEND, 0700);
+	cpTo = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0664);
 	if (cpTo < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[3]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
 	writenChar = write(cpTo, buf, readChar);
 	if (writenChar != readChar)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[3]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
