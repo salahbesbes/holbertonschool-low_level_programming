2ca67c49f18b39541b3b5cf51dc759eaf3a6a2e6
salah_besbes <salah_besbes@ymail.com>
2020-11-09 22:44:50 +0100

    appand file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fileNumber = open(filename, O_WRONLY | O_APPEND);
-	if (fileNumber < 0 || errno == EACCES )
+	if (fileNumber < 0)
 		return (-1);
 	if (!text_content)
 		close(fileNumber);
