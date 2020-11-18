9844ea54cc99902830c3f1023ff6df7ae060c92e
salah_besbes <salah_besbes@ymail.com>
2020-11-18 14:42:45 +0100

    f

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,6 +14,7 @@
 int main(int argc, char *argv[])
 {
 	UNUSED(argv);
+	
 	printf("%d\n", argc - 1);
 	return (0);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,15 +15,9 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-<<<<<<< HEAD
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-void _putchar(char c);
-=======
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 void _putchar(char c);
 
->>>>>>> c2c734b... fix any diff
 #endif
