f105f92e89c3547ccb849b4d7274d24a30223de8
salahbesbes <salah_besbes@ymail.com>
2021-01-17 00:26:55 +0100

    insert element into hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 
 #include "hash_tables.h"
+#include <string.h>
 
 /**
 * add_node - adds a new node at the beginning of a hash_node_t list.
@@ -90,6 +91,19 @@ hash_node_t *add_node_end(hash_node_t **head, const char *key, const char *value
 
 	return (newNode);
 }
+int check_existance(hash_table_t *ht, const char *key, int idx)
+{
+	hash_node_t *current = NULL;
+
+	current = ht->array[idx];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
 /**
   * hash_table_set - append the key value data to the hashed table
   * at the correct index
@@ -103,13 +117,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int idx = 0;
 	hash_node_t *result = NULL;
+	int res  = 0;
 
-	if (!key || *key == '\0' || !value)
+	if (!ht || !key || *key == '\0')
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-
-	result = add_node((hash_node_t **)&(ht->array[idx]), key, value);
+	res = check_existance(ht, key, idx);
+	if (!res)
+		return (0);
+	result = add_node(&(ht->array[idx]), key, value);
 	if (!result)
 		return (0);
 	return (1);
