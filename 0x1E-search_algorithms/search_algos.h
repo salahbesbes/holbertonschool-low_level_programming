0ce224645b0d7e477fb80ec27ae95e7364052311
salahbesbes <salah_besbes@ymail.com>
2021-04-20 14:22:03 +0200

    linear search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,14 +1,16 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
 
-#ifndef INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H_H
-#define INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H_H
+/** HEADERS */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 
+/** Prototype */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int my_linear_search(int *ar, int start, int length, int val);
-int interpolation_search(int *array, size_t size, int value);
-int search(int *ar, int start, int end, int val);
+
 #endif
