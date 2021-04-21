7efe5832f57d6d579e0fa438e0de6d4d954837d5
salahbesbes <salah_besbes@ymail.com>
2021-04-21 17:32:02 +0200

    advanced bnay sch using recursive

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -14,12 +14,15 @@ void print_array(int *ar, int start, int end)
 	int i;
 	char sep = ',';
 
-	printf("Searching in array:");
-	for (i = start; i <= end; i++)
+	if (end >= start)
 	{
-		if (i == end)
-			sep = '\n';
-		printf(" %d%c", ar[i], sep);
+		printf("Searching in array:");
+		for (i = start; i <= end; i++)
+		{
+			if (i == end)
+				sep = '\n';
+			printf(" %d%c", ar[i], sep);
+		}
 	}
 }
 
@@ -39,21 +42,6 @@ int advanced_binary(int *array, size_t size, int value)
 }
 
 /**
- * recur_check_for_occurence - return the first index occurence of a value
- * @arr: array of int sorted
- * @index: indef of val already found
- * @val: val we want search for the
- * Return: first occurence index
- */
-
-int recur_check_for_occurence(int *arr, int index, int val)
-{
-	if (arr[index - 1] != val)
-		return (index);
-	return (recur_check_for_occurence(arr, index - 1, val));
-}
-
-/**
  * search_interval - binary search
  * @ar: array of int
  * @start: start of the segment of the array
@@ -63,28 +51,21 @@ int recur_check_for_occurence(int *arr, int index, int val)
  */
 int search_interval(int *ar, int start, int end, int val)
 {
-	int mid = (end + start) / 2;
-
-	print_array(ar, start, end);
 
 	if (end >= start)
 	{
-		if (val == ar[mid])
-		{
-			return (recur_check_for_occurence(ar, mid, val));
-		}
+		int mid = (end + start) / 2;
 
-		else if (val < ar[mid])
-		{
-			if (search_interval(ar, start, mid - 1, val) != -1)
-				return (recur_check_for_occurence(ar, mid, val));
-			else
-				return (-1);
-		}
-		else
-		{
+		print_array(ar, start, end);
+		if (ar[start] == val)
+			return (start);
+
+		if (val == ar[mid] && ar[mid - 1] != val)
+			return (mid);
+		if (val <= ar[mid])
+			return (search_interval(ar, start, mid, val));
+		if (val > ar[mid])
 			return (search_interval(ar, mid + 1, end, val));
-		}
 	}
 	return (-1);
 }
